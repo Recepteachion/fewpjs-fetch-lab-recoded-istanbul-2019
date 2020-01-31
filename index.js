8b0a59634e93f2a9a4a7377d2e62d2f2c847b378
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-31 11:46:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
 
-    fetch('https://anapioficeandfire.com/api/books')
+    return fetch('https://anapioficeandfire.com/api/books')
     .then(response => response.json())
     .then(renderBooks)
 }
